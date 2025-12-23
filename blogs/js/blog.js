fdceef71e7a0dffaf7b014f3c4a3dc41976591b3
Gigis123 <gigisghifari@gmail.com>
2025-12-23 13:33:09 +0700

    Add Blog JS

---
+++ blogs/js/blog.js
@@ -0,0 +1,30 @@
+fetch("blogs/data/blog.json")
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (blogs) {
+    var grid = document.getElementById("blog-grid");
+    grid.innerHTML = "";
+
+    blogs.forEach(function (blog) {
+      var card = document.createElement("div");
+      card.className = "blog-card";
+
+      card.innerHTML = `
+        <img src="${blog.image}">
+        <div class="blog-content">
+          <span class="blog-category">${blog.category}</span>
+          <h3>${blog.title}</h3>
+          <p>${blog.excerpt}</p>
+          <a href="blogs/blog_detail/blog_detail.html?id=${blog.id}" class="blog-btn">
+            Baca Selengkapnya →
+          </a>
+        </div>
+      `;
+
+      grid.appendChild(card);
+    });
+  })
+  .catch(function (err) {
+    console.error("Blog error:", err);
+  });
---
+++ blogs/js/blog_detail.js
@@ -0,0 +1,29 @@
+const params = new URLSearchParams(window.location.search);
+const blogId = params.get("id");
+
+fetch("../../blogs/data/blog.json")
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (blogs) {
+    const blog = blogs.find(function (b) {
+      return b.id === blogId;
+    });
+
+    const container = document.getElementById("blog-detail");
+
+    if (!blog) {
+      container.innerHTML = "<p>Blog tidak ditemukan.</p>";
+      return;
+    }
+
+    container.innerHTML = `
+      <h1>${blog.title}</h1>
+      <p><strong>${blog.date}</strong> | ${blog.category}</p>
+      <img src="../../${blog.image}" style="max-width:100%; margin:20px 0;">
+      ${blog.content}
+    `;
+  })
+  .catch(function (err) {
+    console.error("Detail error:", err);
+  });
