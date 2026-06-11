07d55eb3221696923b96d3cacdfd0fcfbff7507e
Gigis123 <gigisghifari@gmail.com>
2026-06-11 17:32:04 +0700

    add blogs again

--- blogs/js/blog.js
+++ blogs/js/blog.js
@@ -1,3 +1,8 @@
+const BLOGS_PER_PAGE = 12;
+
+const params = new URLSearchParams(window.location.search);
+const currentPage = parseInt(params.get("page")) || 1;
+
 fetch("blogs/data/blog.json")
   .then(function (res) {
     return res.json();
@@ -5,15 +10,23 @@ fetch("blogs/data/blog.json")
   .then(function (blogs) {
 
     var grid = document.getElementById("blog-grid");
+    var pagination = document.getElementById("pagination");
+
     grid.innerHTML = "";
 
-    blogs.forEach(function (blog) {
+    const start = (currentPage - 1) * BLOGS_PER_PAGE;
+    const end = start + BLOGS_PER_PAGE;
+
+    const pageBlogs = blogs.slice(start, end);
+
+    pageBlogs.forEach(function (blog) {
 
       var card = document.createElement("div");
       card.className = "blog-card";
 
       card.innerHTML = `
         <img src="${blog.image.replace('../../','')}">
+
         <div class="blog-content">
 
           <span class="blog-category">${blog.category}</span>
@@ -42,6 +55,45 @@ fetch("blogs/data/blog.json")
       grid.appendChild(card);
 
     });
+
+    // Create Pagination
+    const totalPages = Math.ceil(
+      blogs.length / BLOGS_PER_PAGE
+    );
+
+    pagination.innerHTML = "";
+
+    if (totalPages > 1) {
+
+      // Previous button
+      if (currentPage > 1) {
+        pagination.innerHTML += `
+          <a href="?page=${currentPage - 1}" class="page-btn">
+            ←
+          </a>
+        `;
+      }
+
+      // Page numbers
+      for (let i = 1; i <= totalPages; i++) {
+
+        pagination.innerHTML += `
+          <a href="?page=${i}"
+             class="page-btn ${i === currentPage ? 'active' : ''}">
+             ${i}
+          </a>
+        `;
+      }
+
+      // Next button
+      if (currentPage < totalPages) {
+        pagination.innerHTML += `
+          <a href="?page=${currentPage + 1}" class="page-btn">
+            →
+          </a>
+        `;
+      }
+    }
 
   })
   .catch(function (err) {
